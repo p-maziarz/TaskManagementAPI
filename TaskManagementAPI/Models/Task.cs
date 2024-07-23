f15daadd9bdb3b53af9aa80714848fab6aff09cb
pmaziarz <parkermaziarz@gmail.com>
2024-07-22 18:52:22 -0700

    Add project files.

---
+++ TaskManagementAPI/Models/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagementAPI.Models
+{
+    public class Task
+    {
+        public int Id { get; set; } // primary key for the Task entity
+        public string Name { get; set; } // name of the task
+        public string Description { get; set; } // description of task
+        public bool IsCompleted { get; set; } // status of the task
+
+    }
+}
